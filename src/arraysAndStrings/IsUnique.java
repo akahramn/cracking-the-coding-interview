06f1ecbdb918b68bcfe1d51df102920a610f33e9
Abdullah Kahraman <a.kahramnn@gmail.com>
2024-03-13 00:58:19 +0300

    .

---
+++ src/arraysAndStrings/IsUnique.java
@@ -0,0 +1,12 @@
+package arraysAndStrings;
+public class IsUnique {
+
+    /**
+     * Implement an algorithm to determine if a string has all
+     * unique characters. What if you can not use additional data structures?
+     * **/
+    public static void main(String[] args) {
+        String value = "abcd";
+
+    }
+}
